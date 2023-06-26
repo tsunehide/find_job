43a1e2e27735dbb722d332f523e3d10f2b2135db
smaru1111 <marusou20@gmail.com>
2023-06-26 15:06:14 +0900

    #6 add ヘッダーコンポーネント作成

--- find-job/src/app/page.tsx
+++ find-job/src/app/page.tsx
@@ -4,6 +4,7 @@ const Home: React.FC = () => {
   return (
     <>
       <h1>Hello World</h1>
+      
     </>
   );
 };
--- find-job/tailwind.config.js
+++ find-job/tailwind.config.js
@@ -6,13 +6,10 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    colors: {
+      primary: "#3FB84B",
+      secondary: "#C9F4CD" ,
+      white: "#FFFFFF", 
   },
-  plugins: [],
+ },
 }
